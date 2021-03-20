b9414ad339335d6fd961017f5aa6b2f255bc1471
KKapoor0801 <58838197+KKapoor0801@users.noreply.github.com>
2021-03-21 00:53:19 +0530

    Login Auth Done

--- js/signup.js
+++ js/signup.js
@@ -1,17 +1,17 @@
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
-    apiKey: "AIzaSyANjvf0unSqOLHfO_etLGEvAfFoPnV5Q2g",
-    authDomain: "crossconnect-1caa9.firebaseapp.com",
-    projectId: "crossconnect-1caa9",
-    storageBucket: "crossconnect-1caa9.appspot.com",
-    messagingSenderId: "1068788031671",
-    appId: "1:1068788031671:web:a03a6d9c126aa3e1fb2d32",
-    measurementId: "G-G3VMZTWP7N"
-  };
-// Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// var firebaseConfig = {
+//     apiKey: "AIzaSyANjvf0unSqOLHfO_etLGEvAfFoPnV5Q2g",
+//     authDomain: "crossconnect-1caa9.firebaseapp.com",
+//     projectId: "crossconnect-1caa9",
+//     storageBucket: "crossconnect-1caa9.appspot.com",
+//     messagingSenderId: "1068788031671",
+//     appId: "1:1068788031671:web:a03a6d9c126aa3e1fb2d32",
+//     measurementId: "G-G3VMZTWP7N"
+//   };
+// // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+//   firebase.analytics();
 //Reference form collection
 var formRef=firebase.database().ref("Users");
 
@@ -33,6 +33,8 @@ function submitForm(e){
 
   //Save form
   saveForm(name,email,phone,address,city,pincode);
+
+  authSignup();
 
   //Show alert
   document.querySelector('.alert').style.display = 'block';
@@ -64,7 +66,7 @@ function saveForm(name,email,phone,address,city,pincode){
 function authSignup(){
 
  var email1=document.getElementById('signupEmail').value;
- var password=document.getElementById('signupPassqord').value;
+ var password=document.getElementById('signupPassword').value;
  
  //Create User with Email and Password
  firebase.auth().createUserWithEmailAndPassword(email1, password).catch(function(error) {
