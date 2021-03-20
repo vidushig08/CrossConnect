cb3bf78e977bfc8685645c984442f8a8fef29f69
Ankita Mandal <66859175+Ankita-Mandal@users.noreply.github.com>
2021-03-20 22:11:39 +0530

    form page update

--- user.js
+++ user.js
@@ -1,14 +1,18 @@
-var coll = document.getElementsByClassName("panel-heading");
-var i;
+// var coll = document.getElementsByClassName("panel-heading");
+// var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
+// for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var content = this.nextElementSibling;
+//     if (content.style.display === "block") {
+//       content.style.display = "none";
+//     } else {
+//       content.style.display = "block";
+//     }
+//   });
+// }
+
+$('.accordian').collapse({
+    toggle: false
+  })
