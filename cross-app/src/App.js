55c6c7625ac1c657c5cd8885baa440d41f635365
Ankita Mandal <66859175+Ankita-Mandal@users.noreply.github.com>
2021-03-20 09:19:31 +0530

    user started

--- cross-app/src/App.js
+++ cross-app/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route } from 'react-router-dom';
 
+import './App.css';
+import Landing from './pages/landing/landing';
+import LogIn from './pages/login/login';
+import Worker from './pages/worker/worker'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <React.Fragment>
+      <div className="App">
+         {/* <Switch> */}
+           <Route exact path='/' component={Landing} />
+           <Route exact path='/ngo' component={LogIn} />
+           <Route exact path='/worker' component={Worker} />
+         {/* </Switch> */}
+       </div>
+     </React.Fragment>
+      <Landing/>
     </div>
+    // <h1>heyy</h1>
+    
   );
 }
 
---
+++ cross-app/src/pages/landing/landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './landing.css'
+
+const Landing = () => {
+    return(
+        
+            <h1 className='title'> CROSSCONNECT </h1>
+        
+    );
+}
+
+export default Landing
